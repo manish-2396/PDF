99a4db34a1195b4ae7ea3c7b5e73c5de65ac5603
manish <manishchouhan2396@gmail.com>
2023-04-23 18:07:06 +0530

    update

--- backend/DataBase/db.js
+++ backend/DataBase/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const server = mongoose.connect("mongodb+srv://manishchouhan:manish@cluster0.gpac4nx.mongodb.net/pdf?retryWrites=true&w=majority")
+const server = mongoose.connect("")
 
 
 module.exports = {
